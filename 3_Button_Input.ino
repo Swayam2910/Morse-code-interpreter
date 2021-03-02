50bb269da076bbeab274539e66d7f9fbb17defb6
Ridhima2727 <78317002+Ridhima2727@users.noreply.github.com>
2021-03-02 21:46:55 +0530

    Update 3_Button_Input.ino

--- 3_Button_Input.ino
+++ 3_Button_Input.ino
@@ -1,6 +1,72 @@
 #include <string.h>
 char str[10]="\0";
+const int Button_Pin1 = 13;#include <string.h>
+char letter [10]="\0";
+char words[50]="\0";
+
+
 const int Button_Pin1 = 13;
+const int Button_Pin2 = 12;
+const int Button_Pin3 = 11;
+String Character[26] ={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+String Alphabets[26]={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","y","Z",};
+
+void setup()
+{
+  pinMode(Button_Pin1,INPUT); 
+  pinMode(Button_Pin2,INPUT); 
+  pinMode(Button_Pin3,INPUT); 
+  Serial.begin(9600);
+}
+
+void decode_morse()
+{
+  for (int i =0;i<26;i++)
+  {
+    if (Character[i]==letter)
+    {
+      const char *A = Alphabets[i].c_str();
+      strncat(words,A,1);
+      memset(&letter[0], 0, sizeof(letter)); //set the string back to zero
+    
+    }
+  }
+}
+
+void Read_from_Serial()
+{
+  int Byte=Serial.read();
+  if (Byte==80||Byte==112)
+  {
+    Serial.println(words);
+    memset(&words[0], 0, sizeof(words)); //set the string back to zero
+  }
+}
+
+void loop()
+{
+  if (digitalRead(Button_Pin1)==HIGH)
+  {
+    Serial.print(".");
+    strncat(letter,".",1);
+    delay(200);
+  }
+  else if (digitalRead(Button_Pin2)==HIGH)
+  {
+    Serial.print("-");
+    strncat(letter,"-",1);
+    delay(200);
+  }
+  else if (digitalRead(Button_Pin3)==HIGH)
+  {
+    Serial.println(" ");
+    delay(200);
+    decode_morse();
+  }
+  Read_from_Serial();
+}
+
+
 const int Button_Pin2=12;
 const int Button_Pin3=11;
 String Character[26] ={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
